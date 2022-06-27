58fefd4c1042b20af4bd5251c2afc34f0a205c5d
Dominic Ruff <dominic.ruff@consult.red>
2022-06-27 15:05:17 +0200

    LCM-90: added copyrights to the new DSM files and installation script

--- integration/rdk-b-22q1/dsm/rpi/scripts/apply_patches_overlays.sh
+++ integration/rdk-b-22q1/dsm/rpi/scripts/apply_patches_overlays.sh
@@ -4,6 +4,22 @@
 #@param[in]     $1      target file directory path [NB: default is "."]
 #@param[in]     $2      patch/new files base directory path containing folders "./patches" and "./new"  [NB: default is "./DSM/integration/rdk-b-22q1/dsm/rpi"]
 
+# If not stated otherwise in this file or this component's license file the
+# following copyright and licenses apply:
+#
+# Copyright 2022 Consult Red
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 function apply_patch(){
 #@description   Applies a patch file, using the 'patch' command given a relative path file name, to the corresponding file in the target directory path
@@ -13,7 +29,7 @@ function apply_patch(){
 
 patch_dir=.
 if [ ! "$3" == "" ]; then
-        patch_dir=$3/$(dirname $1)
+    patch_dir=$3/$(dirname $1)
     if [ ! -d $patch_dir ]; then
         mkdir -p $patch_dir
     fi
